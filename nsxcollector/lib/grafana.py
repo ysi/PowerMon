0fd80cf18156dfdfebdb5d5314dfc2440e43bf5b
Yann Simonet <ysimonet@gmail.com>
2022-08-01 13:45:11 +0200

    Update grafana.py
    create class objects

--- nsxcollector/lib/grafana.py
+++ nsxcollector/lib/grafana.py
@@ -4,6 +4,137 @@ from lib.formatDatas import panelManagerProcess
 from fabric import Connection
 import sys, pprint
 
+
+class grafana:
+    datasource_uid = ''
+    folders = []
+    dashboards = []
+    def __init__(self, name):
+        self.name = name
+
+    def createFolder(self, folder_name, uid):
+        self.folders.append(folder(folder_name, uid))
+
+    def createDashboard(self, name, folder_uid):
+        db = dashboard(name, folder_uid)
+        self.folders.append(db)
+        return db
+
+    def getFolderUID(self, name):
+        # return folder UID by the name
+        for fd in self.folders:
+            if fd.name == name:
+                return fd.uid
+
+    def getInfluxDataSourceUID(self, dictenv):
+        """
+        Get the UID of the influxDB Datasource
+        """
+        grafanaport = dictenv['GRAFANA_PORT']
+        grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
+        grafanalogin = dictenv['GRAFANA_ADMIN_USER']
+        grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
+
+        url = grafanaurl + '/api/datasources/name/' + dictenv['INFLUXDB_DOCKER_CONTAINER_NAME']
+        response, code = connection.GetAPIGeneric(url, grafanalogin, grafanapassword, False)
+        if code != 200:
+            print(color.style.RED + "==> ERROR: " + color.style.NORMAL + "Grafana - Datasource " + dictenv['INFLUXDB_DOCKER_CONTAINER_NAME'] + " not found")
+            sys.exit()
+        else:
+            self.datasource_uid = response['uid']
+
+# Object Class for Grafana Dashboard
+class dashboard:
+    uid = ''
+    panels = []
+    # init method or constructor
+    def __init__(self, name, folder):
+        self.name = name
+        self.folder = folder
+
+    def addPanel(self, panel):
+        self.panels.append(panel)
+
+    def applyDashboard(self, dictenv, panels):
+        """
+        applyDashboard(dictenv, folder, panels)
+        Apply dashboard object in Grafana by REST/API
+
+        Args
+        ----------
+        dictenv : .env dictionary
+        folder: folder uid
+        panels: list of panels in dashboard
+        """
+        grafanaport = dictenv['GRAFANA_PORT']
+        grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
+        grafanalogin = dictenv['GRAFANA_ADMIN_USER']
+        grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
+        url = grafanaurl + '/api/dashboards/db'
+        self.uid = self.name.replace(' ', '')
+        body = {
+                "dashboard": {
+                "title": self.name,
+                "panels": panels,
+                "uid": self.uid,
+                "tags": [ "powermon" ],
+                "timezone": "browser",
+                "schemaVersion": 16,
+                "version": 0,
+                "refresh": "25s"
+                },
+                "folderUid": self.folder,
+                "message": "Created by PowerMon",
+                "overwrite": False
+        }
+        resul, code = connection.GetAPIGeneric(grafanaurl + '/api/dashboards/uid/' + self.uid, grafanalogin, grafanapassword, False)
+        if code != 200:
+            connection.PostAPIGeneric(url, grafanalogin, grafanapassword, body, True, 'Grafana', 'Create Dashboard ' + self.name)
+        else:
+            print(color.style.RED + "==> " + color.style.NORMAL + "Grafana - Dashboard " + self.name + " already existing")
+        
+        return self.uid
+
+
+# Object Class for Grafana Folder
+class folder:
+    dashboard = ''
+    def __init__(self, name, uid):
+        self.uid = uid
+        self.name = name
+
+    def applyFolder(self, dictenv):
+        """
+        applyFolder(dictenv)
+        apply in grafana a folder by request API
+
+        Args
+        ----------
+        dictenv : .env dictionary
+        """
+        body = {
+            "uid": self.uid,
+            "title": self.name
+        }
+        grafanaport = dictenv['GRAFANA_PORT']
+        grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
+        grafanalogin = dictenv['GRAFANA_ADMIN_USER']
+        grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
+        url = grafanaurl + '/api/folders'
+
+        result, code = connection.GetAPIGeneric(url + '/' + self.uid, grafanalogin, grafanapassword, False)
+        if code != 200:
+            connection.PostAPIGeneric(url, grafanalogin, grafanapassword, body, True, 'Grafana', 'Create folder ' + self.name)
+        else:
+            print(color.style.RED + "==> " + color.style.NORMAL + "Grafana - Folder " + self.name + " already existing")
+
+
+# Object Class for Grafana Panel
+class panel:
+    def __init__(self, title):
+        self.title = title
+
+
 def createGrafanaEnv(config, dictenv, ListTN, Commands_List):
     """
     createGrafanaEnv(dictenv, ListTN)
@@ -14,9 +145,14 @@ def createGrafanaEnv(config, dictenv, ListTN, Commands_List):
     dictenv : .env dictionary
     ListTN : List of Transport Node Object
     """
+
+    # init grafana object
+    gf = grafana('PowerMon')
     # Create Folder
-    folder = createFolder(dictenv, dictenv['COMPOSE_PROJECT_NAME'])
-    influxdbuid = getInfluxDataSourceUID(dictenv)
+    gf.createFolder(dictenv['COMPOSE_PROJECT_NAME'], 'PowerMonPowerMon')
+    for fd in gf.folders:
+        fd.applyFolder(dictenv)
+    gf.getInfluxDataSourceUID(dictenv)
 
     # Create list of type of equipment
     Component = []
@@ -32,100 +168,14 @@ def createGrafanaEnv(config, dictenv, ListTN, Commands_List):
             Component.append('NSX Managers')
             # Create panels for each commands and each Component
         if tn.type == 'Manager':
-            panels = createPanel(tn.cmd, influxdbuid, dictenv['INFLUXDB_DB'], config)
-    # Create Dasboard for each type of component
-    for cp in Component:
-        createGrafanaDashboard(dictenv, cp, folder, panels)
-
-
-
-def createFolder(dictenv, folder):
-    """
-    createFolder(dictenv, folder)
-    Create a grafana folder
-
-    Args
-    ----------
-    dictenv : .env dictionary
-    folder : name of the folder
-    """
-    grafanaport = dictenv['GRAFANA_PORT']
-    grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
-    grafanalogin = dictenv['GRAFANA_ADMIN_USER']
-    grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
-    url = grafanaurl + '/api/folders'
-    uid = 'PowerMonPowerMon'
-    body = {
-        "uid": uid,
-        "title": folder
-    }
-    result, code = connection.GetAPIGeneric(url + '/' + uid, grafanalogin, grafanapassword, False)
-    if code != 200:
-        connection.PostAPIGeneric(url, grafanalogin, grafanapassword, body, True, 'Grafana', 'Create folder ' + folder)
-    else:
-        print(color.style.RED + "==> " + color.style.NORMAL + "Grafana - Folder " + folder + " already existing")
-
-    return uid
-
-
-def createGrafanaDashboard(dictenv, name, folderuid, panels):
-    """
-    createGrafanaDashboard(dictenv, name, folderuid)
-    Create a dashboard inside a folder
-
-    Args
-    ----------
-    dictenv : .env dictionary
-    name : name of the dashboard
-    folderuid : UID of the folder
-    panels: list of panels in dashboard
-    """
-    grafanaport = dictenv['GRAFANA_PORT']
-    grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
-    grafanalogin = dictenv['GRAFANA_ADMIN_USER']
-    grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
-    url = grafanaurl + '/api/dashboards/db'
-    uid = name.replace(' ', '')
-    body = {
-            "dashboard": {
-              "title": name,
-              "panels": panels,
-              "uid": uid,
-              "tags": [ "powermon" ],
-              "timezone": "browser",
-              "schemaVersion": 16,
-              "version": 0,
-              "refresh": "25s"
-            },
-            "folderUid": folderuid,
-            "message": "Created by PowerMon",
-            "overwrite": False
-    }
-    resul, code = connection.GetAPIGeneric(grafanaurl + '/api/dashboards/uid/' + uid, grafanalogin, grafanapassword, False)
-    if code != 200:
-        connection.PostAPIGeneric(url, grafanalogin, grafanapassword, body, True, 'Grafana', 'Create Dashboard ' + name)
-    else:
-        print(color.style.RED + "==> " + color.style.NORMAL + "Grafana - Dashboard " + name + " already existing")
+            panels = createPanel(tn.cmd, gf.datasource_uid, dictenv['INFLUXDB_DB'], config)
     
-    return uid
+    # Create Dasboard for each type of component
+    fd_uid = gf.getFolderUID(dictenv['COMPOSE_PROJECT_NAME'])
+    for cp in Component:
+        db = gf.createDashboard(cp,fd_uid)
+        db.applyDashboard(dictenv, panels)
 
-
-def getInfluxDataSourceUID(dictenv):
-    """
-    Get the UID of the influxDB Datasource
-    """
-    grafanaport = dictenv['GRAFANA_PORT']
-    grafanaurl = "http://" + dictenv['GRAFANA_NAME'] + ":" + grafanaport
-    grafanalogin = dictenv['GRAFANA_ADMIN_USER']
-    grafanapassword = dictenv['GRAFANA_ADMIN_PASSWORD']
-
-    url = grafanaurl + '/api/datasources/name/' + dictenv['INFLUXDB_DOCKER_CONTAINER_NAME']
-    response, code = connection.GetAPIGeneric(url, grafanalogin, grafanapassword, False)
-    if code != 200:
-        print(color.style.RED + "==> ERROR: " + color.style.NORMAL + "Grafana - Datasource " + dictenv['INFLUXDB_DOCKER_CONTAINER_NAME'] + " not found")
-        sys.exit()
-    else:
-        return response['uid']
 
 
 def createPanel(Commands_List, influxdbuid, infludbname, config):
